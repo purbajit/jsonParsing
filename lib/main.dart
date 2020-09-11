989290d181589539b08effcc34be547d3200d8e6
PURBAJIT DAS <purbajitdas@PURBAJITs-MacBook-Pro.local>
2020-09-12 02:46:19 +0530

    final commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:jsonparsing/pages/homePage.dart';
+import 'package:splashscreen/splashscreen.dart';
 
-void main() {
-  runApp(MyApp());
+void main() => runApp(MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyApp(),
+    ));
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
 }
 
-class MyApp extends StatelessWidget {
+class _MyAppState extends State<MyApp> {
   // This widget is the root of our application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Homescreen',
-      theme: ThemeData(
-        primarySwatch: Colors.teal,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(),
+    return SplashScreen(
+      seconds: 4,
+      backgroundColor: Colors.black,
+      image: Image.asset('assets/giphy.gif'),
+      loaderColor: Colors.white,
+      photoSize: 260.0,
+      navigateAfterSeconds: MyHomePage(),
     );
   }
 }
