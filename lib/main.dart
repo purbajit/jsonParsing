36942a9ad4a96beb941cc6a57557bbfaaddc97ad
PURBAJIT DAS <purbajitdas@PURBAJITs-MacBook-Pro.local>
2020-09-12 02:19:37 +0530

    final commit

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Homescreen',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.teal,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: MyHomePage(),
